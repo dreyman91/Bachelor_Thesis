ab347ba42dcb5a7ae7324a320185628805953691
Oluwadamilare Adegun <adegun_dare@icloud.com.com>
2025-03-31 19:54:10 +0200

    update

---
+++ Wrapper/tests/model_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from Wrapper.src.Comm_Wrapper.comm_failure_api_v1 import ActiveCommunication
+
+agents = ["agent_0", "agent_1"]
+comm = ActiveCommunication(agent_ids=agents)
+
+print("Matrix dtype:", comm.matrix.dtype)
+print("Initial Matrix:", comm.get_state())
+
+comm.update("agent_0", "agent_1", False)
+print("Can agent_0 talk to agent_1?", comm.can_communicate("agent_0", "agent_1"))
+
+print("Blocked for agent_0:", comm.get_blocked_agents("agent_0"))  # Expect: ['agent_1']
+
+
+comm.reset()
+print("After reset:\n", comm.get_state())
